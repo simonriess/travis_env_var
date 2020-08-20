b7d4ecc5d752a816f7037ab74049f3d5d2780b30
Simon Rieß <riess.simon@yahoo.de>
2020-08-20 12:18:44 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function main() {
+    const favoriteFood = process.env.FAVORITE_FOOD;
+  while(true) {
+    console.log("Microservices rock!");
+    console.log(`My favorite food is ${favoriteFood}`);
+    await sleep(5000);
+  }
+}
+
+main();
